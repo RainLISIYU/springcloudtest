ce9bca6c31c238769f29070545d769cf27a0fac3
liangsiyu01 <liangsiyu01@inspur.com>
2021-10-20 19:00:07 +0800

    配置修改

--- springcloudclientthree/src/main/java/com/example/springcloudclientthree/SpringcloudclientthreeApplication.java
+++ springcloudclientthree/src/main/java/com/example/springcloudclientthree/SpringcloudclientthreeApplication.java
@@ -1,12 +1,22 @@
 package com.example.springcloudclientthree;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SpringcloudclientthreeApplication {
+public class SpringcloudclientthreeApplication implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudclientthreeApplication.class, args);
